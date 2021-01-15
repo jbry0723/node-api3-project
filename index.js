a83c89f8de18edb7ecc29b8c9adbc1822df44a54
Justin Justin <jbry0723@gmail.com>
2021-01-14 21:14:17 -0500

    changed for deployment

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // require your server and start it
+require('dotenv').config()
 const server=require('./api/server')
 
-server.listen(4000, () => {
+const PORT=process.env.PORT || 4000
+
+server.listen(PORT, () => {
     console.log('\n* Server Running on http://localhost:4000 *\n');
   });
